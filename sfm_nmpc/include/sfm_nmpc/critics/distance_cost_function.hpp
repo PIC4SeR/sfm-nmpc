317062d5b649d3065c6ae23aed90e045b0ec62bb
Andrea Ostuni <andrea.ostuni@polito.it>
2026-03-05 12:32:33 +0000

    Update copyright information and license details across multiple files
    - Updated copyright year to 2026 and changed license from Apache-2.0 to GNU General Public License 3.0 in header comments for:
      - social_work_cost_function.hpp
      - velocity_cost_function.hpp
      - velocity_feasibility_cost_function.hpp
      - obstacle_distance_interface.hpp
      - optimizer.hpp
      - path_trajectorizer.hpp
      - people_interface.hpp
      - sfm.hpp
      - social_mpc_controller.hpp
      - motion_model.hpp
      - path_handler.hpp
      - regulated_pure_pursuit.hpp
      - type_definitions.hpp
      - trajectory_memory.hpp
      - update_state.hpp
      - agent_angle_cost_function.cpp
      - angle_cost_function.cpp
      - crossing_cost_function.cpp
      - curvature_cost_function.cpp
      - distance_cost_function.cpp
      - goal_align_cost_function.cpp
      - obstacle_cost_function.cpp
      - proxemics_cost_function.cpp
      - social_work_cost_function.cpp
      - velocity_cost_function.cpp
      - velocity_feasibility_cost_function.cpp
      - obstacle_distance_interface.cpp
      - optimizer.cpp
      - path_handler.cpp
      - path_trajectorizer.cpp
      - people_interface.cpp
      - social_mpc_controller.cpp
      - test_path_trajectorizer.cpp

--- sfm_nmpc/include/sfm_nmpc/critics/distance_cost_function.hpp
+++ sfm_nmpc/include/sfm_nmpc/critics/distance_cost_function.hpp
@@ -1,3 +1,19 @@
+// Copyright 2026 PIC4SeR - Politecnico di Torino and SRL - Service Robotics Lab, Pablo de Olavide University
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #ifndef MPC_SFM_MOTION_MODEL__DISTANCE_COST_FUNCTION_HPP_
 #define MPC_SFM_MOTION_MODEL__DISTANCE_COST_FUNCTION_HPP_
 
--- sfm_nmpc/src/critics/crossing_cost_function.cpp
+++ sfm_nmpc/src/critics/crossing_cost_function.cpp
@@ -1,4 +1,20 @@
-/#include "sfm_nmpc/critics/crossing_cost_function.hpp"
+// Copyright 2026 PIC4SeR - Politecnico di Torino and SRL - Service Robotics Lab, Pablo de Olavide University
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+#include "sfm_nmpc/critics/crossing_cost_function.hpp"
 
 namespace sfm_nmpc
 {
